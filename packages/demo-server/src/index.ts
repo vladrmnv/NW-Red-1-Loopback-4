91f6e92fed4adddd01260486e7c3ac25f11b17f9
[Vlad] <[vr.loudness@gmail.com]>
2018-10-12 21:21:03 -0400

    demo server init

---
+++ packages/demo-server/src/index.ts
@@ -0,0 +1,16 @@
+import {DemoServerApplication} from './application';
+import {ApplicationConfig} from '@loopback/core';
+
+export {DemoServerApplication};
+
+export async function main(options: ApplicationConfig = {}) {
+  const app = new DemoServerApplication(options);
+  await app.boot();
+  await app.start();
+
+  const url = app.restServer.url;
+  console.log(`Server is running at ${url}`);
+  console.log(`Try ${url}/ping`);
+
+  return app;
+}
---
+++ packages/demo-server/test/acceptance/test-helper.ts
@@ -0,0 +1,24 @@
+import {DemoServerApplication} from '../..';
+import {
+  createRestAppClient,
+  givenHttpServerConfig,
+  Client,
+} from '@loopback/testlab';
+
+export async function setupApplication(): Promise<AppWithClient> {
+  const app = new DemoServerApplication({
+    rest: givenHttpServerConfig(),
+  });
+
+  await app.boot();
+  await app.start();
+
+  const client = createRestAppClient(app);
+
+  return {app, client};
+}
+
+export interface AppWithClient {
+  app: DemoServerApplication;
+  client: Client;
+}
